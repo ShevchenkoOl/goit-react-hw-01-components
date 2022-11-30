8a1c5c7da25dc6872b06e45c67d8f940b0deb013
Oleksii Shevchenko <uzlabini@gmail.com>
2022-11-30 20:36:50 +0100

    1212212

--- src/components/transaction/Transaction.js
+++ src/components/transaction/Transaction.js
@@ -25,9 +25,9 @@ export const Transaction = ({ items }) => (
 Transaction.propType = {
   items: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      id: PropTypes.string.isRequired,
       type: PropTypes.string.isRequired,
-      amount: PropTypes.number.isRequired,
+      amount: PropTypes.string.isRequired,
       currency: PropTypes.string.isRequired,
     })
   ).isRequired,
